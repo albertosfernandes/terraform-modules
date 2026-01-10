d4f2b694dfce5355655431f2c971786e134196a4
albertosfernandes <alberto.souzafernandes@gmail.com>
2026-01-10 17:34:22 -0300

    Service sem ALB

--- aws/resources/ecs/service/main.tf
+++ aws/resources/ecs/service/main.tf
@@ -9,20 +9,10 @@ resource "aws_ecs_service" "this" {
 
   launch_type = "FARGATE"
 
-  deployment_controller {
-    type = "CODE_DEPLOY"
-  }
-
   network_configuration {
     subnets         = var.subnets
     security_groups = var.security_groups
     assign_public_ip = var.assign_public_ip
-  }
-
-  load_balancer {
-    target_group_arn = var.target_group_arn
-    container_name   = "app"
-    container_port   = 8000
   }
 
   lifecycle {
